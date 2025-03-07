774090c98b2f802e80c96461b71573617547226c
abhijit <abhijitlimited1@gmail.com>
2025-03-07 07:35:18 +0530

    add some small changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,12 @@
-import { render } from 'preact'
-import './index.css'
-import { App } from './app.jsx'
+import { render } from "preact";
+import { App } from "./app.jsx";
+import "./index.css";
+import ReactGA from "react-ga4";
 
-render(<App />, document.getElementById('app'))
+// Initialize Google Analytics
+ReactGA.initialize("G-P59WNG5LRY");
+
+// Track page view
+ReactGA.send("pageview");
+
+render(<App />, document.getElementById("app"));
